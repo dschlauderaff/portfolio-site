15a77093dacd3c70731d38e9084aa90732130a0e
Dave Schlauderaff <dschlaud@gmail.com>
2018-01-05 20:56:04 -0600

    removes index.css and adds twin peaks typography theme

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,13 +1,14 @@
 import React from 'react'
 import Link from 'gatsby-link'
+import Container from '../components/container'
 
 const IndexPage = () => (
-  <div>
+  <Container>
     <h1>Hi people</h1>
     <p>Welcome to your new Gatsby site.</p>
     <p>Now go build something great.</p>
     <Link to="/blog/">Go to the blog</Link>
-  </div>
+  </Container>
 )
 
 export default IndexPage
--- src/utils/typography.js
+++ src/utils/typography.js
@@ -1,5 +1,6 @@
 import Typography from 'typography'
+import twinPeaksTheme from 'typography-theme-twin-peaks'
 
-const typography = new Typography({ baseFontSize: "36px"})
+const typography = new Typography(twinPeaksTheme)
 
 export default typography
